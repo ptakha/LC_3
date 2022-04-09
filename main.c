bb037502c1e13442304bf427408867f90702843a
ptakha <potoka1998@gmail.com>
2022-04-09 20:05:58 +0200

    Added main and neighboring functions +sign_extend
    Added:
    * main loop
    * read_image()
    * read_image_file()
    * mem_read()
    * mem_write()
    * swap16()
    * sign_extend()

--- main.c
+++ main.c
@@ -1,4 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+#include <signal.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/termios.h>
+#include <sys/mman.h>
 
 #define MEMORY_MAX (1 << 16)
 uint16_t memory[MEMORY_MAX];
@@ -13,7 +26,7 @@ enum {
   R_PC,
   R_COND,
   R_COUNT
-};
+} Registers;
 
 uint16_t reg[R_COUNT];
 
@@ -34,10 +47,151 @@ enum {
   OP_RES,   //
   OP_LEA,   // Load effective address
   OP_TRAP   // Execute trap
-};
+} Opcodes;
 
 enum {
   FL_POS = 1 << 0, // Positive flag, 1
   FL_ZRO = 1 << 1, // Zero flag, 2
   FL_NEG = 1 << 2  //
-};
+} Flags;
+
+enum { PC_START = 0x3000};
+
+
+uint16_t swap16(uint16_t x)
+{
+  x = (x << 8)|(x >> 8);
+  return x;
+}
+
+void read_image_file(FILE* file)
+{
+  // Where in memory place image
+  uint16_t origin;
+  fread(&origin, sizeof(origin), 1, file);
+  origin = swap16(origin);
+
+  uint16_t max_read = MEMORY_MAX-origin;
+  uint16_t* p = memory+origin;
+  size_t read = fread(p, sizeof(uint16_t), max_read, file);
+  while(read > 0)
+  {
+    *p = swap16(*p);
+    ++p;
+    read--;
+  }
+
+}
+
+int read_image(const char* path)
+{
+  FILE* file  = fopen(path, "rb");
+  if (!file)
+  {
+    return 0;
+  }
+  read_image_file(file);
+  fclose(file);
+  return 1;
+}
+
+uint16_t sign_extend(uint16_t x, int bitcount)
+{
+  // we use 16 bits per value of x in calculations,
+  // but in ADD instruction we get only 5 bits for immediate value
+  //
+  // x is from 1 0000 = -15 to 0 1111 = 15
+  // -14 in 5 bits = 1 0001
+  // ((1 0001) >> 4) & 1 =
+  // 1 0001 >> 4 = 1 & 1 = 1 => x is negative
+  //
+  // 1111 1111 1110 0000
+  //|             1 0001
+  // -------------------
+  // 1111 1111 1111 0001 = -14
+  //
+  //
+  if ((x >> (bitcount - 1)) & 1)
+  {
+    x = x | (0xFFFF << bitcount);
+  }
+  return x;
+}
+
+void mem_write(uint16_t address, uint16_t value)
+{
+  memory[address] = value;
+}
+
+uint16_t mem_read(uint16_t address)
+{
+  return memory[address];
+}
+
+int main(int argc, const char* argv[])
+{
+
+  if (argc<2)
+  {
+    printf("Usage:\n lc3 [image-file-1]...\n");
+    exit(2);
+  }
+  for (int j=1;j<argc;j++)
+  {
+    if(!read_image(argv[j]))
+    {
+      printf("Failed to load image %s\n", argv[j]);
+      exit(1);
+    }
+  }
+  // Set Z flag
+  reg[R_COND] = FL_ZRO;
+  // Set Program Counter to 0x3000
+  reg[R_PC] = PC_START;
+  bool running = true;
+  uint16_t instr;
+  uint16_t op;
+  while (running)
+  {
+    // Fetch instruction
+    instr = mem_read(reg[R_PC]++);
+    op = instr >> 12;
+    switch (op) {
+      case OP_BR:
+      break;
+      case OP_ADD:
+      break;
+      case OP_LD:
+      break;
+      case OP_ST:
+      break;
+      case OP_JSR:
+      break;
+      case OP_AND:
+      break;
+      case OP_LDR:
+      break;
+      case OP_STR:
+      break;
+      case OP_RTI:
+      break;
+      case OP_NOT:
+      break;
+      case OP_LDI:
+      break;
+      case OP_STI:
+      break;
+      case OP_JMP:
+      break;
+      case OP_RES:
+      break;
+      case OP_LEA:
+      break;
+      case OP_TRAP:
+      break;
+      default:
+      printf("Bad opcode - %d", op);
+      break;
+    }
+  }
+}
