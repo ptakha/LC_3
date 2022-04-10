09d004dcdd9a7d9235ac212c8429b5782e3e63d5
ptakha <potoka1998@gmail.com>
2022-04-10 13:09:57 +0200

    LEA

--- main.c
+++ main.c
@@ -299,6 +299,10 @@ int main(int argc, const char* argv[])
       }
       case OP_LEA:
       {
+        uint16_t r0 = (instr >> 9) & 0x7;
+        uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+        reg[r0] = reg[R_PC]+pc_offset;
+        update_flags(r0);
         break;
       }
       case OP_TRAP:
